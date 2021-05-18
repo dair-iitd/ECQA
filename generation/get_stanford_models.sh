d4bd9d64071212509a75d9f6ad7d4e85129bd5b1
divyanshu092 <48377772+divyanshu092@users.noreply.github.com>
2021-05-18 12:15:19 +0530

    removed formatting issue

--- generation/get_stanford_models.sh
+++ generation/get_stanford_models.sh
@@ -2,10 +2,7 @@
 # This script downloads the Stanford CoreNLP models.
 
 CORENLP=stanford-corenlp-full-2015-12-09
-SPICELIB=/spice/lib
-
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $DIR
+SPICELIB=./spice/lib
 
 echo "Downloading..."
 
